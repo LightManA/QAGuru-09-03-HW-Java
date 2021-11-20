0e0d92a56a5d2fc2fa507fc58aeea85828dcc681
LightManA <AwarenessLightA@gmail.com>
2021-11-20 23:59:57 +0600

    This is created two classes and objects into it and inputting of the info about three separate objects (aquarium fishs)

---
+++ src/main/java/com/github/LightManA/AquariumFish.java
@@ -0,0 +1,51 @@
+package com.github.LightManA;
+
+public class AquariumFish {
+    String nickName; // имя
+    String family; // семейство, к которому относится рыба
+    String type; // вид, к которому относится рыба
+    String coloration = "yellow"; // окраска
+    double size; // размер (длина)
+    int age; // возраст, в месяцах
+
+    public void whatIsYourNameMyFish() {
+        System.out.println("My name is: " + nickName);
+    }
+
+    public void determineFamilyAndTypeOfFish() {
+        System.out.println("Family: " + family);
+        System.out.println("Type: " + type);
+    }
+
+    // определение текущего размера рыбы
+    public void determiningCurrentFishSize() {
+        System.out.println("Current size: " + size);
+    }
+
+    // рыба взрослеет
+    public void determiningCurrentAge() {
+        System.out.println("Current age: " + age);
+    }
+
+    // рыба взрослеет
+    public void fishMatures() {
+        age = age + 3;
+        System.out.println("I grew up to: " + age);
+    }
+
+    // меняется окраска рыбы при ее взрослении
+    public void changingColorOfFishAsTheyGrowUp() {
+        System.out.println("Current coloration: " + coloration);
+    }
+
+    // рыба питается
+    public void fishEat() {
+        System.out.println("Am-am-am...");
+    }
+
+    // рыба питается
+    public void bigScaryFishEats() {
+        System.out.println("Khyrch-khyrch-khyrch...");
+    }
+
+}
---
+++ src/main/java/com/github/LightManA/QAGuru09HomeWorkJava.java
@@ -0,0 +1,65 @@
+package com.github.LightManA;
+
+public class QAGuru09HomeWorkJava {
+
+    public static void main(String[] args) {
+
+        AquariumFish doris = new AquariumFish();
+        AquariumFish rybus = new AquariumFish();
+        AquariumFish monstrus = new AquariumFish();
+
+        doris.nickName = "Doris";
+        rybus.nickName = "Rybus";
+        monstrus.nickName = "Monstrus";
+
+        doris.family = "Characidae";
+        doris.type = "Gymnocorymbus ternetzi";
+        rybus.family = "Minnows and Carps";
+        rybus.type = "Epalzeorhynchos bicolor (labeo)";
+        monstrus.family = "Armored catfishes";
+        monstrus.type = "Ancistrus dolichopterus";
+
+        doris.size = 3.25;
+        rybus.size = 12.7;
+        monstrus.size = 13.99;
+
+        doris.age = 39;
+        rybus.age = 30;
+        monstrus.age = 57;
+
+        doris.coloration = "red";
+        rybus.coloration = "grey";
+        monstrus.coloration = "pink";
+
+        System.out.println();
+
+        doris.whatIsYourNameMyFish();
+        doris.determineFamilyAndTypeOfFish();
+        doris.determiningCurrentFishSize();
+        doris.fishMatures();
+        doris.changingColorOfFishAsTheyGrowUp();
+        doris.fishEat();
+
+        System.out.println();
+
+        rybus.whatIsYourNameMyFish();
+        rybus.determineFamilyAndTypeOfFish();
+        rybus.determiningCurrentFishSize();
+        rybus.determiningCurrentAge();
+        rybus.changingColorOfFishAsTheyGrowUp();
+        doris.fishEat();
+
+        System.out.println();
+
+        monstrus.whatIsYourNameMyFish();
+        monstrus.determineFamilyAndTypeOfFish();
+        monstrus.determiningCurrentFishSize();
+        monstrus.determiningCurrentAge();
+        monstrus.changingColorOfFishAsTheyGrowUp();
+        monstrus.bigScaryFishEats();
+
+        System.out.println();
+
+    }
+
+}
